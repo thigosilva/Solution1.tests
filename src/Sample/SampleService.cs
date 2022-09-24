88eb1a293225cf570bb5e01abc210d32be77ebca
Gilvan Durães Galindo <gilvan.dgalindo@sp.senac.br>
2022-09-23 21:01:40 -0300

    Adicionar arquivos de projeto.

---
+++ src/Sample/SampleService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sample
+{
+    public class SampleService
+    {
+
+        public bool IsPrime(int candidate) 
+        {
+
+            if (candidate < 2)
+            {
+                return false;
+            }
+
+            for (int divisor = 2; divisor < Math.Sqrt(candidate); divisor++)
+            {
+                if (candidate % divisor == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ tests/Sample.Test/SampleServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+namespace Sample.Test
+{
+    public class SampleServiceTest
+    {
+        private readonly SampleService _sampleService;
+
+        public SampleServiceTest()
+        {
+            _sampleService = new SampleService();
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _sampleService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void IsPrime_PrimesLessThan10_ReturnTrue(int value)
+        {
+            var result = _sampleService.IsPrime(value);
+
+            Assert.True(result, $"{value} should be prime");
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void IsPrime_NonPrimesLessThan10_ReturnFalse(int value)
+        {
+            var result = _sampleService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
